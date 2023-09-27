387db33f31d512e6766398fe355bc0ca4df0b891
cchance6 <cchance6@gatech.edu>
2023-09-26 22:12:52 -0400

    Changed file extensions, ubtated babel.config.js.

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+
+import NewUserPage from './components/NewUserPage';
+import HomePage from './components/HomePage';
+
+function App() {
+  return <HomePage />;
+}
+
+export default App;
