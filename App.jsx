b6359ddaf530b594dfba678d6937c6d589435286
chamilton15 <cphamilton1000@yahoo.com>
2023-11-01 16:46:33 -0400

    Player Profile Screen
    Shows player profile screen with dummy data

--- App.jsx
+++ App.jsx
@@ -3,11 +3,21 @@ import {NavigationContainer} from '@react-navigation/native';
 
 import MyStack from './navigation/MyStack';
 
+import PlayerProfileScreen from './screens/Profile/PlayerProfileScreen';
+
 function App() {
+  const dummy = {
+    name: 'Charlie',
+    skillRating: 99,
+    position: 'Wing',
+    height: '6ft 3in',
+    weight: '185 lbs',
+    playType: 'Playmaking Sharpshooter',
+    age: 20,
+    profileImage: { uri: 'stripes' }
+  };
   return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
+    <PlayerProfileScreen userInfo={dummy}></PlayerProfileScreen>
   );
 }
 
