e7b911be80699c68720be0c1804419e0c4bb640f
chamilton15 <cphamilton1000@yahoo.com>
2023-11-01 16:59:40 -0400

    Revert "Player Profile Screen"
    This reverts commit b6359ddaf530b594dfba678d6937c6d589435286.

--- App.jsx
+++ App.jsx
@@ -3,21 +3,11 @@ import {NavigationContainer} from '@react-navigation/native';
 
 import MyStack from './navigation/MyStack';
 
-import PlayerProfileScreen from './screens/Profile/PlayerProfileScreen';
-
 function App() {
-  const dummy = {
-    name: 'Charlie',
-    skillRating: 99,
-    position: 'Wing',
-    height: '6ft 3in',
-    weight: '185 lbs',
-    playType: 'Playmaking Sharpshooter',
-    age: 20,
-    profileImage: { uri: 'stripes' }
-  };
   return (
-    <PlayerProfileScreen userInfo={dummy}></PlayerProfileScreen>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
 
