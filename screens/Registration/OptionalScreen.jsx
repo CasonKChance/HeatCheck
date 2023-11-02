749f1b94899507a6e823521365bb5d934152a9d7
chamilton15 <cphamilton1000@yahoo.com>
2023-11-02 13:19:47 -0400

    Revert "Merge branch 'Charlie' of https://github.com/CasonKChance/HeatCheck into Charlie"
    This reverts commit f2e0bf9000df5cba2fecf74fe114d439e89ac64a, reversing
    changes made to b6359ddaf530b594dfba678d6937c6d589435286.

--- screens/Registration/OptionalScreen.jsx
+++ screens/Registration/OptionalScreen.jsx
@@ -4,83 +4,49 @@ import {
   TouchableOpacity,
   View,
   Text,
+  TextInput,
   SafeAreaView,
   StyleSheet,
 } from 'react-native';
-import RNPickerSelect from 'react-native-picker-select';
+
+const submitHandler = () => {};
 
 const OptionalScreen = ({navigation, route}) => {
-  const [userData, setUserData] = useState({
-    ...route.params?.userData,
-    position: undefined,
-    hometown: undefined,
-    skillLevel: undefined,
-    height: undefined,
-    weight: undefined,
-    ageGroup: undefined,
-  });
-
-  const skillLevelOptions = [
-    {label: 'Professional / Division 1', value: '90-99'},
-    {label: 'Collegiate', value: '80-89'},
-    {label: 'Varsity', value: '70-79'},
-    {label: 'Junior Varsity', value: '60-69'},
-    {label: 'Casual', value: '50-59'},
-    {label: 'Beginner', value: '40-49'},
-  ];
-
-  const ageGroupOptions = [
-    {label: '18-25', value: '18-25'},
-    {label: '26-35', value: '26-35'},
-    {label: '36-45', value: '36-45'},
-  ];
-
-  const positionOptions = [
-    {label: 'Point Guard', value: '1'},
-    {label: 'Combo Guard', value: '1/2'},
-    {label: 'Shooting Guard', value: '2'},
-    {label: 'Wing', value: '2/3'},
-    {label: 'Small Forward', value: '3'},
-    {label: 'Point Forward', value: '3/4'},
-    {label: 'Power Forward', value: '4'},
-    {label: 'Big Man', value: '4/5'},
-    {label: 'Center', value: '5'},
-  ];
-
-  const renderPicker = (label, field, items) => {
-    return (
-      <View style={styles.inputContainer}>
-        <Text>{label}</Text>
-        <RNPickerSelect
-          onValueChange={value => setUserData({...userData, [field]: value})}
-          items={items}
-        />
-      </View>
-    );
-  };
-
+  const [userData, setUserData] = useState(route.params?.userData);
   return (
     <BackgroundWrapperContainer>
       <SafeAreaView>
-        <View style={styles.container}>
+        <View style={{paddingHorizontal: 25}}>
           <Text style={styles.header}>Optional</Text>
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Text>Back</Text>
+          <View style={styles.inputContainer}>
+            <TextInput placeholder="First Name" style={styles.emailField} />
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput placeholder="Last Name" style={styles.emailField} />
+          </View>
+
+          <TouchableOpacity style={styles.loginButton} onPress={submitHandler}>
+            <Text style={styles.loginButtonText}>Submit</Text>
           </TouchableOpacity>
-          {renderPicker(
-            'Pick Your Skill Level',
-            'skillLevel',
-            skillLevelOptions,
-          )}
-          {renderPicker('Pick Your Age Group', 'ageGroup', ageGroupOptions)}
-          {renderPicker('Pick Your Position', 'position', positionOptions)}
-          <TouchableOpacity
-            style={styles.submitButton}
-            onPress={() => {
-              navigation.navigate('Start');
-            }}>
-            <Text style={styles.submitButtonText}>Submit</Text>
-          </TouchableOpacity>
+          <View style={styles.newUserContainer}>
+            <Text
+              style={{
+                color: 'rgba(43, 45, 66, 1)',
+                fontWeight: '700',
+                fontFamily: 'BakbakOne-Regular',
+              }}>
+              Already registered?
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('Login');
+              }}>
+              <Text style={{color: 'rgba(239,35,60,1)', fontWeight: '700'}}>
+                {' '}
+                Login
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </SafeAreaView>
     </BackgroundWrapperContainer>
@@ -88,16 +54,12 @@ const OptionalScreen = ({navigation, route}) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 25,
-  },
   header: {
     fontFamily: 'BakbakOne-Regular',
     fontSize: 28,
     fontWeight: '500',
     marginBottom: 30,
     color: 'rgba(43,45,66,1)',
-    marginTop: 20,
   },
   inputContainer: {
     flexDirection: 'row',
@@ -106,18 +68,45 @@ const styles = StyleSheet.create({
     marginBottom: 25,
     color: 'rgba(43, 45, 66, 1)',
   },
-  submitButton: {
+  emailField: {
+    fontFamily: 'BakbakOne-Regular',
+    flex: 1,
+    paddingVertical: 0,
+    color: 'rgba(43,45,66,1)',
+  },
+  passwordField: {
+    fontFamily: 'BakbakOne-Regular',
+    flex: 1,
+    paddingVertical: 0,
+  },
+  forgot: {
+    color: 'rgba(239,35,60,1)',
+    fontFamily: 'BakbakOne-Regular',
+    fontWeight: '700',
+  },
+  loginButton: {
     backgroundColor: 'rgba(239,35,60,1)',
     padding: 20,
     borderRadius: 10,
     marginBottom: 30,
   },
-  submitButtonText: {
+  loginButtonText: {
     fontFamily: 'BakbakOne-Regular',
     textAlign: 'center',
     fontWeight: '700',
     color: 'rgba(237,242,244,1)',
     fontSize: 16,
+  },
+  otherLoginText: {
+    fontFamily: 'BakbakOne-Regular',
+    textAlign: 'center',
+    color: 'rgba(43,45,66,1)',
+    marginBottom: 30,
+  },
+  newUserContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 30,
   },
 });
 
