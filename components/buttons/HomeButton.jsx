01d400557bc81f806839e3ea5e6e05ca072175b1
cchance6 <cchance6@gatech.edu>
2023-11-21 16:13:18 -0500

    Added Home Screen and updated package dependent files

---
+++ components/buttons/HomeButton.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+const HomeButton = ({onPress}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={[styles.container]}>
+        <View style={styles.rect3}>
+          <Text style={styles.home}>Home</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  rect3: {
+    marginTop: 100,
+    width: width / 1.35,
+    height: height / 12.5,
+    backgroundColor: 'rgba(39,45,45,1)',
+    borderRadius: 40,
+    shadowColor: 'rgba(43,45,66,1)',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    elevation: 60,
+    shadowOpacity: 1,
+    shadowRadius: 20,
+    top: height / 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  home: {
+    fontFamily: 'BakbakOne-Regular',
+    color: 'rgba(255,255,255,1)',
+    fontSize: 20,
+  },
+});
+
+export default HomeButton;
