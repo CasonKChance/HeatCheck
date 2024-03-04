337c1e66b01415897522710a0a5b43929aab8921
Verzino, Andrew Michael <averzino3@gatech.edu>
2024-03-04 13:02:17 -0500

    idk.

---
+++ assets/stylesheets/MapStyles.jsx
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  mapContainer: {
+    flexDirection: 'row',
+    margin: 20,
+    marginTop: 50,
+    marginBottom: 10,
+    height: 250,
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default styles;
